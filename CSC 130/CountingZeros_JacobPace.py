2cc4a90bc977c9ddc6f51ca1673a2930f98c118e
JacobPace <127350081+JacobPace@users.noreply.github.com>
2023-04-14 14:05:14 -0500

    Delete CSC 130 directory

--- CSC 130/CountingZeros_JacobPace.py
+++
@@ -1,9 +0,0 @@
-numList = []
-counter = 0
-for i in range(1,1000000,1):
-    numList.append(i)
-numStr = str(numList)
-for n in numStr:
-    countZeros = int(str(n.count("0")))
-    counter += countZeros
-print(counter)
