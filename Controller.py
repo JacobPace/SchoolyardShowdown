40358c5b8afc261b9326ff48e7c5e156b3e4098a
JacobPace <127350081+JacobPace@users.noreply.github.com>
2023-05-11 17:12:05 -0500

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,2 +1,8 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(BCM)
+GPIO.setmode(GPIO.BCM)
+UpButton = 17
+DownButton = 16
+SelectButton = 13
+GPIO.setup(UpButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(DownButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(SelectButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
