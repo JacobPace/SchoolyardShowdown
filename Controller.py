68dd63030ad24e162a0119256c56a2b7d060fa67
JacobPace <127350081+JacobPace@users.noreply.github.com>
2023-05-11 17:16:44 -0500

    Delete Controller.py

--- Controller.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-UpButton = 17
-DownButton = 16
-SelectButton = 13
-GPIO.setup(UpButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(DownButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(SelectButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#need a change to commit nahudjkawbksjBDkjsAJKDASJ
